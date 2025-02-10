bb2bfabf1494c9cc97edbeed1cb10b09caf465d6
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-02-10 16:57:20 +0500

    Update python.py

--- python/python.py
+++ python/python.py
@@ -1,49 +1,49 @@
-# ism = input("ismingiz nima?\t >>>")
-# if ism.lower() != 'sanjar':
-#     print(f"Uzr, {ism.title()} biz Sanjarni kutyabmiz")
-# else:
-#     print("Salom,Sanjar")    
+ism = input("ismingiz nima?\t >>>")
+if ism.lower() != 'sanjar':
+    print(f"Uzr, {ism.title()} biz Sanjarni kutyabmiz")
+else:
+    print("Salom,Sanjar")    
 
 
-# javob = float(input("12*6 nechiga teng"))
-# if javob != 72:
-#     print("javob xato")
-# else:
-#     print("javob togri")    
+javob = float(input("12*6 nechiga teng"))
+if javob != 72:
+    print("javob xato")
+else:
+    print("javob togri")    
 
-# yosh = int(input("Yoshingiz nechidaa?>>>"))
-# if yosh >= 18:
-#     print('Xush kelibsiz')
-# else:
-#     print('Kirish mumkin emas')    
-
-
-
-# login = input("Yangi login tanlang")
-# if len(login)<=5:
-#     print("login 5harfdan koproq bolishi krak")
-# else:
-#     print(f"{login} login sifatida qabul qilindi")   7
+yosh = int(input("Yoshingiz nechidaa?>>>"))
+if yosh >= 18:
+    print('Xush kelibsiz')
+else:
+    print('Kirish mumkin emas')    
 
 
-# yosh = int(input("Yoshingizni kiriting:"))
-# if yosh < 18:
-#     print("Siz {yosh}yoshda ekansiz sizga kirish bepul")
-# elif 18 <= yosh <= 35:
-#     print("Siz {yosh}yoshda ekansiz sizga kirish narxi 10000som")
+
+login = input("Yangi login tanlang")
+if len(login)<=5:
+    print("login 5harfdan koproq bolishi krak")
+else:
+    print(f"{login} login sifatida qabul qilindi")   7
+
+
+yosh = int(input("Yoshingizni kiriting:"))
+if yosh < 18:
+    print("Siz {yosh}yoshda ekansiz sizga kirish bepul")
+elif 18 <= yosh <= 35:
+    print("Siz {yosh}yoshda ekansiz sizga kirish narxi 10000som")
     
-# else:
-#     print("Siz {yosh}yoshda ekansiz kirish narxi 5000som ")
+else:
+    print("Siz {yosh}yoshda ekansiz kirish narxi 5000som ")
 
 
 
-# a = int(input("1-soni kiriting:"))
-# b = int(input("2-soni kiriting:"))
-# d = int(input("3-soni kiriting:"))
+a = int(input("1-soni kiriting:"))
+b = int(input("2-soni kiriting:"))
+d = int(input("3-soni kiriting:"))
 
-# son = [a,b,d]
-# son.sort()
-# print(son)
+son = [a,b,d]
+son.sort()
+print(son)
 
 login = input("Loginingizni kiriting")
 parol = input("Parolingizni kiriting")
