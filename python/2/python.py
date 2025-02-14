6c8838cf18001b02df0bfa281affdb29125f4a01
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-02-14 15:42:33 +0500

    Update python.py

--- python/2/python.py
+++ python/2/python.py
@@ -1,1 +1,12 @@
-foydalanuvchi = int(input)
+foydalanuvchi = int(input("Bahoni kiriting:"))
+
+if foydalanuvchi >= 90:
+    print("A")
+elif foydalanuvchi >=80:
+    print("B")
+elif foydalanuvchi >= 70:
+    print("C")        
+elif foydalanuvchi >=60:
+    print("D")
+else:
+    print("F")        
