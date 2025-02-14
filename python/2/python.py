21ecc59f51a15e9c44c2275dddf43d1246b526b7
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-02-14 15:53:04 +0500

    Update python.py

--- python/2/python.py
+++ python/2/python.py
@@ -1,12 +1,24 @@
-foydalanuvchi = int(input("Bahoni kiriting:"))
+# foydalanuvchi = int(input("Bahoni kiriting:"))
 
-if foydalanuvchi >= 90:
-    print("A")
-elif foydalanuvchi >=80:
-    print("B")
-elif foydalanuvchi >= 70:
-    print("C")        
-elif foydalanuvchi >=60:
-    print("D")
+# if foydalanuvchi >= 90:
+#     print("A")
+# elif foydalanuvchi >=80:
+#     print("B")
+# elif foydalanuvchi >= 70:
+#     print("C")        
+# elif foydalanuvchi >=60:
+#     print("D")
+# else:
+#     print("F")        
+
+
+n = int(input("Ixtiyoriy son kiriting"))
+
+if n % 3 == 0:
+    print("Fizz")
+elif n%5==0:
+    print("Buzz")
+elif n%3==0 and n%5==0:
+    print("FizzBuzz")
 else:
-    print("F")        
+    print(n)            
