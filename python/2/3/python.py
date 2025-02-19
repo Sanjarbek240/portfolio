7ef62a28d175b04bb9fe02ddab550c64f531dfa2
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-02-19 15:59:24 +0500

    Update python.py

--- python/2/3/python.py
+++ python/2/3/python.py
@@ -2,4 +2,29 @@ menu = ['osh','somsa','shorva','lavash']
 buyurtmalar = []
 narx = 0
 x = int(input("Hurmatli mijoz siz nechta taom buyurma qilasiz:\t"))
-for in
+for i in range(0,x):
+    buyurtma = input(f"{i+1}buyurma kiriting:\t")
+    buyurtmalar.append(buyurtma)
+
+if buyurtmalar:
+    for taom in buyurtmalar:
+        if taom in menu:
+            print(f"sizning {taom.title()} nomli buyurtmangiz qabul qilindi")
+        else:
+            print(f"sizning{taom.title()}nomli buyurtmangiz mavjud emas!")
+else:
+    print("Savatchangiz bosh")
+print(f"\nSizning buyurmalar soningiz: {len(buyurtmalar)}")
+
+if buyurtma == 'osh':
+    narx += 10000
+if buyurtma == 'somsa':
+    narx+=15000
+if buyurtma == 'shorva':
+    narx += 20000
+if buyurtma == 'lavash':
+    narx += 25000
+
+print(f"sizning tolovingiz: {narx}")
+
+
