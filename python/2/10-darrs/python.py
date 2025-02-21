43b7acde4a330587671d3c1e81be72c2289afff8
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-02-21 15:28:50 +0500

    Update python.py

--- python/2/10-darrs/python.py
+++ python/2/10-darrs/python.py
@@ -1,8 +1,28 @@
-son = int(input("Ixtiyoriy son kiriting:"))
+# son = int(input("Ixtiyoriy son kiriting:"))
 
-if son > 0:
-    print("Bu son musbat >",son)
-elif son < 0:
-    print("Bu son manfiy >",son)
+# if son > 0:
+#     print("Bu son musbat >",son)
+# elif son < 0:
+#     print("Bu son manfiy >",son)
+# else:
+#     print("Bu son 0 ga teng")        
+
+
+# hafta = int(input("Hafta kunlarini raqam bilan kiriting:"))
+
+# if 1 <= hafta <= 5:
+#     print("Bu ish kuni")
+# elif 6 <= hafta <=7:
+#     print("Bu dam olish kuni")
+# else:
+#     print("Siz kiritgan raqam xato")   
+
+bir = int(input("Birinchi sonni kiriting:"))
+ikki = int(input("ikkinchi sonni kiriting:"))
+
+if bir > ikki:
+    print("Birinchi son ikkinchisidan katta")
+elif bir < ikki:
+    print("Birinchi son Ikkinchi sondan kichik")
 else:
-    print("Bu son 0 ga teng")        
+    print("Bu sonlar bir bir bilan teng")        
