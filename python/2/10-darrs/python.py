0064f244ca917effb408a3363ff1c62e88a8064c
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-02-21 15:35:26 +0500

    Update python.py

--- python/2/10-darrs/python.py
+++ python/2/10-darrs/python.py
@@ -17,12 +17,26 @@
 # else:
 #     print("Siz kiritgan raqam xato")   
 
-bir = int(input("Birinchi sonni kiriting:"))
-ikki = int(input("ikkinchi sonni kiriting:"))
+# bir = int(input("Birinchi sonni kiriting:"))
+# ikki = int(input("ikkinchi sonni kiriting:"))
 
-if bir > ikki:
-    print("Birinchi son ikkinchisidan katta")
-elif bir < ikki:
-    print("Birinchi son Ikkinchi sondan kichik")
+# if bir > ikki:
+#     print("Birinchi son ikkinchisidan katta")
+# elif bir < ikki:
+#     print("Birinchi son Ikkinchi sondan kichik")
+# else:
+#     print("Bu sonlar bir bir bilan teng")   
+
+
+yil = int(input("oylarni kiriting raqamlarda:"))
+
+if 12<= yil <= 2:
+    print("Qish")
+elif 3 <= yil <= 5:
+    print("Bahor")
+elif 6 <= yil <=8:
+    print("Yoz") 
+elif 9<= yil <= 11:
+    print("Kuz")
 else:
-    print("Bu sonlar bir bir bilan teng")        
+    print("Xato")              
