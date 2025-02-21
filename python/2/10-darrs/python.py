eeb3b73858d112a5100e0f1c4da191464a83a8ef
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-02-21 15:14:54 +0500

    Update python.py

--- python/2/10-darrs/python.py
+++ python/2/10-darrs/python.py
@@ -1,1 +1,8 @@
 son = int(input("Ixtiyoriy son kiriting:"))
+
+if son > 0:
+    print("Bu son musbat >",son)
+elif son < 0:
+    print("Bu son manfiy >",son)
+else:
+    print("Bu son 0 ga teng")        
