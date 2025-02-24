85d61c59cc7ac196b3d9779e4504ccc1dfd47d08
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-02-24 15:57:17 +0500

    Create python.py

---
+++ python/2/lugat/11 dars/python.py
@@ -0,0 +1,10 @@
+f = {
+    "brand" : "ford",
+    "electric" : False,
+    "year" : 1964,
+    "color" : ["white","black","red"]
+
+}
+
+for t,e in f.items():
+    print(f"Kalit : {t} Qiymat : {e}")
