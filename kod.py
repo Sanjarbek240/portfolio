6f095e747a853eeed07586708c94a0044801149a
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-02-24 16:07:41 +0500

    s

---
+++ kod.py
@@ -0,0 +1,7 @@
+kitoblar = {
+        "Python" : "Guido van Rossum",
+        "Java" : "James Gosling",
+        "C++" : "Bjarne Stroustrup"
+}
+
+print(f"{kitoblar}")
--- python/2/lugat/11 dars/python.py
+++ python/2/lugat/11 dars/python.py
@@ -1,15 +1,29 @@
-f = {
-    "brand" : "ford",
-    "electric" : False,
-    "year" : 1964,
-    "color" : ["white","black","red"]
+# f = {
+#     "brand" : "ford",
+#     "electric" : False,
+#     "year" : 1964,
+#     "color" : ["white","black","red"]
 
-}
+# }
 
-for t,e in f.items():
-    if t == "color":
-        print(f"t")
-        for rang in e:
-            print(f"          -{rang}") 
-    else:       
-        print(f"{t}  {e}")
+# for t,e in f.items():
+#     if t == "color":
+#         print(f"t")
+#         for rang in e:
+#             print(f"          -{rang}") 
+#     else:       
+#         print(f"{t}  {e}")
+
+
+#1
+
+# kitoblar = {
+#         "Python" : "Guido van Rossum",
+#         "Java" : "James Gosling",
+#         "C++" : "Bjarne Stroustrup"
+# }
+
+# print(f"{kitoblar}")
+
+
+#2
