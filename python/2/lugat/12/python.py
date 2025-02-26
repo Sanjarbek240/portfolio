c1c88486e0285e73429ac17d1931d2aaf704eadc
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-02-26 16:01:15 +0500

    Update python.py

--- python/2/lugat/12/python.py
+++ python/2/lugat/12/python.py
@@ -8,17 +8,24 @@
 
 
 
-kitoblar = [
-    {"nomi": "UFQ", "muallif": "Otkir Hoshimov", "yili": 1984},
-    {"nomi": 1984, "muallif": "Jorj Oruell", "yili": 1949},
-    {"nomi": "Allkimyogar", "muallif": "Pauleo Kalyuelolo", "yili": 1988},
-    {"nomi": "Dunyo Ishlari", "muallif": "Otkir Hoshimov", "yili": 2011},
-    {"nomi": "Kecha va kunduz", "muallif": "Cholpon", "yili": 1936}
+# kitoblar = [
+#     {"nomi": "UFQ", "muallif": "Otkir Hoshimov", "yili": 1984},
+#     {"nomi": 1984, "muallif": "Jorj Oruell", "yili": 1949},
+#     {"nomi": "Allkimyogar", "muallif": "Pauleo Kalyuelolo", "yili": 1988},
+#     {"nomi": "Dunyo Ishlari", "muallif": "Otkir Hoshimov", "yili": 2011},
+#     {"nomi": "Kecha va kunduz", "muallif": "Cholpon", "yili": 1936}
+# ]
+
+
+# print("Kitoblar ro'yxati:\n")
+# for kitob in kitoblar:
+#     print(f"Nomi: {kitob['nomi']}")
+#     print(f"Muallif: {kitob['muallif']}")
+#     print(f"Chop etilgan yili: {kitob['yili']}")
+
+
+cars = [
+    {"turi": "Malibu", "rang":"oq","narxi":"350000"},
+    {"turi": "Spark", "rang":"qizil","narxi":"12000"}
+    
 ]
-
-
-print("Kitoblar ro'yxati:\n")
-for kitob in kitoblar:
-    print(f"Nomi: {kitob['nomi']}")
-    print(f"Muallif: {kitob['muallif']}")
-    print(f"Chop etilgan yili: {kitob['yili']}")
