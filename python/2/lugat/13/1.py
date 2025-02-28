11e89bee349b8dbfb161abaa85a531fad7ed1f64
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-02-28 16:44:36 +0500

    Update 1.py

--- python/2/lugat/13/1.py
+++ python/2/lugat/13/1.py
@@ -28,18 +28,36 @@
 
 # print(soni)
 
-a = {'a': 3, 'b': 8, 'c': 1, 'd': 6, 'e': 2}
+# a = {'a': 3, 'b': 8, 'c': 1, 'd': 6, 'e': 2}
 
-b = list(a.items()) 
-n = len(b)
+# b = list(a.items()) 
+# n = len(b)
 
-for i in range(n):
-    for d in range(n - 1):
-        if b[d][1] > b[d + 1][1]:  
-            b[d], b[d + 1] = b[d + 1], b[j]  
+# for i in range(n):
+#     for d in range(n - 1):
+#         if b[d][1] > b[d + 1][1]:  
+#             b[d], b[d + 1] = b[d + 1], b[j]  
 
-lugat = {}
-for kalit, qiymat in b:
-    lugat[kalit] = qiymat
+# lugat = {}
+# for kalit, qiymat in b:
+#     lugat[kalit] = qiymat
 
-print("tartiblangan:", lugat)
+# print("tartiblangan:", lugat)
+
+
+
+
+
+kontaklar = {
+    'husan' : 975216686,
+    'sanjar': 948300425
+}
+
+tanlov = input("1 - Yangi Kontakt ,  2 - Kontakt qidirish")
+
+if tanlov == '1':
+    kontaklar[input("Ism kiriting")] = input("Telefon raqam kiriting")
+    print("Kontakt qowildi")
+
+if tanlov == 2:
+    print(kontaklar.get(input("Qidirilgan ish"), "Bunday kontakt yoq"))    
