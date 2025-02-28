bf09d2681e16f50c3f8f826a342f7bff1f7e1faf
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-02-28 15:32:55 +0500

    Update 1.py

--- python/2/lugat/13/1.py
+++ python/2/lugat/13/1.py
@@ -6,3 +6,16 @@
 #         b = c
 
 # print("uzuni:", b)
+
+
+
+lugat = {"olma": 50, "banan": 20, "apelsin": 30}
+
+nomi = input("Mahsulot nomi: ").lower()
+miqdori = int(input("Miqdor: "))
+
+if nomi in lugat and lugat[nomi] >= miqdori:
+    lugat[nomi] -= miqdori
+    print(f"{lugat} dona {nomi} sotildi. Qoldiq: {lugat[nomi]}")
+else:
+    print("Yetarli mahsulot yo‘q!")
