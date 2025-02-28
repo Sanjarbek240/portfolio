5feaadb2e443a1f665dab9f2a503d03c828936c3
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-02-28 16:20:45 +0500

    Update 1.py

--- python/2/lugat/13/1.py
+++ python/2/lugat/13/1.py
@@ -28,3 +28,18 @@
 
 # print(soni)
 
+a = {'a': 3, 'b': 8, 'c': 1, 'd': 6, 'e': 2}
+
+b = list(a.items()) 
+n = len(b)
+
+for i in range(n):
+    for d in range(n - 1):
+        if b[d][1] > b[d + 1][1]:  
+            b[d], b[d + 1] = b[d + 1], b[j]  
+
+lugat = {}
+for kalit, qiymat in b:
+    lugat[kalit] = qiymat
+
+print("tartiblangan:", lugat)
