1f6bf177cf64772128f52979f53f649d247c19c3
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-02-28 15:22:21 +0500

    Update 1.py

--- python/2/lugat/13/1.py
+++ python/2/lugat/13/1.py
@@ -1,8 +1,8 @@
-a = {"first": "developer", "second": "pythonista", "third": "coder"}
+# a = {"first": "developer", "second": "pythonista", "third": "coder"}
 
-b = list(a.values())[0]
-for c in a.values():
-    if len(c) > len(b):
-        b = c
+# b = list(a.values())[0]
+# for c in a.values():
+#     if len(c) > len(b):
+#         b = c
 
-print("uzuni:", b)
+# print("uzuni:", b)
