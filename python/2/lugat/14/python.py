1c288f75286b7d2c16d9cd82f011e3fe9161a8f3
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-03-03 15:55:43 +0500

    Update python.py

--- python/2/lugat/14/python.py
+++ python/2/lugat/14/python.py
@@ -1,12 +1,12 @@
-while True:
-    yosh = int(input("Yoshingizni kiriting (Dastur toxtashi uchun 0 kiriting): "))
+# while True:
+#     yosh = int(input("Yoshingizni kiriting (Dastur toxtashi uchun 0 kiriting): "))
     
-    if yosh == 0:
-        print("dastur tugadi.")
-        break
-    elif yosh < 18:
-        print("hali kichkina ekansiz.")
-    else:
-        print("Xush kelibsiz.")
+#     if yosh == 0:
+#         print("dastur tugadi.")
+#         break
+#     elif yosh < 18:
+#         print("hali kichkina ekansiz.")
+#     else:
+#         print("Xush kelibsiz.")
 
 
