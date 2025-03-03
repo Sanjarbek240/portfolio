6e269c378bd5b9de47cc2f49fc39ccdba471329c
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-03-03 16:10:02 +0500

    Update python.py

--- python/2/lugat/14/python.py
+++ python/2/lugat/14/python.py
@@ -1,23 +1,37 @@
-# while True:
-#     yosh = int(input("Yoshingizni kiriting (Dastur toxtashi uchun 0 kiriting): "))
+while True:
+    yosh = int(input("Yoshingizni kiriting (Dastur toxtashi uchun 0 kiriting): "))
     
-#     if yosh == 0:
-#         print("dastur tugadi.")
-#         break
-#     elif yosh < 18:
-#         print("hali kichkina ekansiz.")
-#     else:
-#         print("Xush kelibsiz.")
+    if yosh == 0:
+        print("dastur tugadi.")
+        break
+    elif yosh < 18:
+        print("hali kichkina ekansiz.")
+    else:
+        print("Xush kelibsiz.")
 
 
-# kitoblar = []
+kitoblar = []
 
-# while True:
-#     kitob = input("Sevimli kitobingizni kiriting: ")
+while True:
+    kitob = input("Sevimli kitobingizni kiriting: ")
     
-#     if kitob.lower() == "stop":
-#         break
-#     kitoblar.append(kitob)
-# print("kiritilgan kitoblar:")
-# for kitob in kitoblar:
-#     print(kitob)
+    if kitob.lower() == "stop":
+        break
+    kitoblar.append(kitob)
+print("kiritilgan kitoblar:")
+for kitob in kitoblar:
+    print(kitob)
+
+
+import random 
+tasodifiy_son = random.randint(1,10)
+while True:
+    try:
+        taxmin = int(input("1 dan 10gacha bolgan sonni kiriting "))
+        if taxmin == tasodifiy_son:
+            print("Tabriklaymiz togri topdingiz")
+            break
+        else:
+            print("Xato")
+    except ValueError:
+        print("Iltimos faqat butun son kiriting")        
