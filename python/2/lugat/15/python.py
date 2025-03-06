cdf89bce63eca3ef003cd1111cb36f5b04dc7905
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-03-06 11:58:10 +0500

    Update python.py

--- python/2/lugat/15/python.py
+++ python/2/lugat/15/python.py
@@ -1,12 +1,21 @@
-ismlar = []
-while True:
-    ism = input("Ismingizni kiriting (toxtatish uchun 'stop' deb yozing): ")
+# ismlar = []
+# while True:
+#     ism = input("Ismingizni kiriting (toxtatish uchun 'stop' deb yozing): ")
     
-    if ism.lower() == "stop": 
-        break
+#     if ism.lower() == "stop": 
+#         break
     
-    ismlar.append(ism)  
+#     ismlar.append(ism)  
 
-print("\nKiritilgan ismlar royxati:")
-for ism in ismlar:
-    print(ism)
+# print("\nKiritilgan ismlar royxati:")
+# for ism in ismlar:
+#     print(ism)
+
+mevalar = ["olma", "banan", "olma", "shaftoli", "olma"]
+
+# "olma" ro‘yxatda bor ekan, uni o‘chiramiz
+while "olma" in mevalar:
+    mevalar.remove("olma")
+
+# Natijaviy ro‘yxatni chiqaramiz
+print("Yangilangan ro‘yxat:", mevalar)
