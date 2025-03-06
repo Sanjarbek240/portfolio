73f03c0acf6a3eae5d998325ba344954b4aef846
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-03-06 12:06:59 +0500

    Update python.py

--- python/2/lugat/15/python.py
+++ python/2/lugat/15/python.py
@@ -16,3 +16,16 @@
 # while "olma" in mevalar:
 #     mevalar.remove("olma")
 # print(mevalar)
+
+talabalar = ["Ali","Vali","Hasan"]
+n = 0
+baholar = {}
+
+while n < len(talabalar):
+    talaba = talabalar[n]
+    n = 1+n
+    ball = int(input(f"{talaba}ning bahosini kiriting:"))
+    baholar[talaba] = ball
+print("\n Talabalar baxosi")
+for i,b in baholar.items():
+    print(f"{i}ning bahosi {b}")
