242be40f72528412487375e3db8275fb1e8f4527
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-03-10 15:20:50 +0500

    Update python.py

--- python/2/lugat/16/python.py
+++ python/2/lugat/16/python.py
@@ -1,6 +1,13 @@
-a = int(input("son kiriting: "))
-b = [i for i in range(1, a) if a % i == 0]
-if sum(b) == a:
-    print("mukammal son")
-else:
-    print("mukammal son emas")
+# a = int(input("son kiriting: "))
+# b = [i for i in range(1, a) if a % i == 0]
+# if sum(b) == a:
+#     print("mukammal son")
+# else:
+#     print("mukammal son emas")
+
+
+while True:
+    try:
+        a = int(input("son kiriting:"))
+        if a % 2 == 0:
+            
