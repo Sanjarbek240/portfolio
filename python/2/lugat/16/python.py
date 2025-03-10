9c002eee28be88d65fb5f9cf717169bea8873214
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-03-10 15:26:20 +0500

    Update python.py

--- python/2/lugat/16/python.py
+++ python/2/lugat/16/python.py
@@ -6,8 +6,13 @@
 #     print("mukammal son emas")
 
 
-while True:
-    try:
-        a = int(input("son kiriting:"))
-        if a % 2 == 0:
-            
+# while True:
+#     try:
+#         a = int(input("son kiriting:"))
+#         if a % 2 == 0:
+#             print("Bu son juft son")
+#         else:
+#             print("Bu son toq")
+#             break
+#     except ValueError:
+#         print("butun son kiriiting")        
