0efc96ec3493e4b2aa6de523ef3cffba2acac1ef
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-03-26 15:04:05 +0500

    Update python.py

---
+++ python/2/lugat/shaxmat taxtasi/python.py
@@ -0,0 +1,5 @@
+# Shaxmat taxtasida harakatlanayotgan ot ning yurish yo‘lini topadigan dastur yozing. Dastur:
+# ✅ 8x8 taxta yaratishi kerak.
+# ✅ Otni boshlang‘ich nuqtaga qo‘yishi kerak.
+# ✅ Barcha yurishlarni topish uchun rekursiv algoritm ishlatishi kerak.
+# ✅ While, for va def dan foydalanib, kodni to‘liq bajarishi kerak.
