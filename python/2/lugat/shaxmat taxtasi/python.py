1be375e6174bc8c561b4c7c4c9f55dbf4a251821
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-03-26 16:05:26 +0500

    Update python.py

--- python/2/lugat/shaxmat taxtasi/python.py
+++ python/2/lugat/shaxmat taxtasi/python.py
@@ -5,37 +5,13 @@
 # ✅ While, for va def dan foydalanib, kodni to‘liq bajarishi kerak.
 
 
-# def shaxmat():
-#     for i in range(8):
-#         for j in range(8):
-#             if (i + j) % 2 == 0:
-#                 print("⬜", end=" ") 
-#             else:
-#                 print("⬛", end=" ")  
-#         print()
+def shaxmat():
+    for i in range(8):
+        for j in range(8):
+            if (i + j) % 2 == 0:
+                print("⬜", end=" ") 
+            else:
+                print("⬛", end=" ")  
+        print()
 
-# shaxmat()
-
-
-def shaxmat_taxtasi():
-    taxta = [[0 for _ in range(8)] for _ in range(8)]
-    return taxta
-
-def ot_harakatlari(x, y, taxta):
-    if x < 0 or x >= 8 or y < 0 or y >= 8 or taxta[x][y] != 0:
-        return
-    taxta[x][y] = 1  # Ot joylashgan joy
-    for dx, dy in [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]:
-        ot_harakatlari(x + dx, y + dy, taxta)
-    taxta[x][y] = 0  # Orqaga qaytishi
-
-# Asosiy funksiya
-def asosiy_funksiya():
-    taxta = shaxmat_taxtasi()
-    boshlangich_x, boshlangich_y = 0, 0  # Yuqoridan chapga
-    ot_harakatlari(boshlangich_x, boshlangich_y, taxta)
-    for qator in taxta:
-        print(" ".join(str(x) for x in qator))
-
-asosiy_funksiya()
-
+shaxmat()
