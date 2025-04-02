ad8077a8b4e457279db26d687d3ce0dbdb587622
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-04-02 17:07:13 +0500

    Update 18.py

--- python/2/lugat/shaxmat taxtasi/18.py
+++ python/2/lugat/shaxmat taxtasi/18.py
@@ -1,5 +1,16 @@
-import math
-radius = 7
-yuza = math.pi * math.pow(radius,2)
-uzunlik = 2 * math.pi * radius
-print(f"yuzasi: {yuza} uzunligi:{uzunlik}")
+# import math
+# foydalanuvchi =float(input("doira yuzasini kiriitng"))
+# yuza =math.pi*foydalanuvchi**2
+# uzunlik =2*math.pi*foydalanuvchi
+# print(f"uzunlik:{uzunlik} yuzasi {yuza}")
+
+
+import random
+
+foydalanuvchi = int(input("1dan 100gacha bolgan sonlardan bittasini tanlang"))
+a = random.randint(1,100)
+
+if foydalanuvchi >= a:
+    print("Siz yutdingiz")
+else:
+    print("yutqazdingiz")    
