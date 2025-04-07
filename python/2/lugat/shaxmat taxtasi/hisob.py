23565f0fd9ee0585362b864eb15983d3c1fb7e53
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-04-07 17:34:06 +0500

    .

--- python/2/lugat/shaxmat taxtasi/hisob.py
+++ python/2/lugat/shaxmat taxtasi/hisob.py
@@ -1,26 +1,16 @@
 def qoshish(a,b):
-    print(a+b)
+    return a+b
 def ayirish(q,w):
-    print(q-w)
+    return q-w
 def kopaytirish(r,e):
-    print(r*e)
+    return r*e
 def bolish(t,u):
-    print(t/u)
+    return t/u
 def kvadrat(i):
-    printi(**2)  
+    return i**2  
 def kvadrat(o):
-    print(o**2)
+    return o**2
 def kub(p):
-    print(p**3)
+    return p**3
 def kub(s):
-    print(s**3)  
-x = float(input("1-son"))  
-y= float(input("2-son"))
-qoshish(x,y)
-ayirish(x,y)
-kopaytirish(x,y)
-bolish(x,y)
-kvadrat(x)
-kvadrat(y)
-kub(x)
-kub(y)
+    return s**3
