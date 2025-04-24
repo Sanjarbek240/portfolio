b72e57a1f2804c3b02dda42e2934bfcb6abd4406
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-04-24 21:33:24 +0500

    .

--- python/2/amaliy/main.py
+++ python/2/amaliy/main.py
@@ -1,1 +1,18 @@
-import hisob
+from a import mahsulotlar
+
+narxlar = []
+while True:
+    nom = input("Mahsulot nomini kiriting: ").lower()
+    if nom in mahsulotlar:
+        narx = mahsulotlar[nom]
+        narxlar.append(narx)
+        print(f"{nom.title()} narxi {narx} so'm.")
+    else:
+        print("Kechirasiz, bu mahsulot mavjud emas.")
+    
+    davom_et = input("Yana mahsulot tanlaysizmi? (ha/yo'q): ").lower()
+    if davom_et != "ha":
+        break
+
+jami = sum(narxlar)
+print(f"\nSiz uchun to'lov miqdori: {jami} so'm.")
