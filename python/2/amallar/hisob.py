1c1b8e1f62364ab852bf7bceb9613e3022f5b395
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-04-24 20:28:02 +0500

    .

---
+++ python/2/amallar/hisob.py
@@ -0,0 +1,26 @@
+def qoshish(a,b):
+    print(a+b)
+def ayirish(q,w):
+    print(q-w)
+def kopaytirish(r,e):
+    print(r*e)
+def bolish(t,u):
+    print(t/u)
+def kvadrat(i):
+    printi(**2)  
+def kvadrat(o):
+    print(o**2)
+def kub(p):
+    print(p**3)
+def kub(s):
+    print(s**3)  
+x = float(input("1-son"))  
+y= float(input("2-son"))
+qoshish(x,y)
+ayirish(x,y)
+kopaytirish(x,y)
+bolish(x,y)
+kvadrat(x)
+kvadrat(y)
+kub(x)
+kub(y)
