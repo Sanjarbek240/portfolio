9768050711b75eadc17bb1b2899e2155b01bd08f
Sanjarbek240 <159249273+Sanjarbek240@users.noreply.github.com>
2025-04-28 15:33:31 +0500

    Update math_utils.py

--- python/2/New folder/1-topshiriq/math_utils.py
+++ python/2/New folder/1-topshiriq/math_utils.py
@@ -1,3 +1,12 @@
 import math
 a = float(input("1-son kiriting"))
 b = float(input("2-son kiriting"))
+c = input("+,-,*,/")
+if c=='+':
+    javob =math.add(a,b)
+elif c =='-':
+    javob= math.subtracta(a,b)        
+elif c =='*':
+    javob= math.multiply(a,b)  
+elif c =='/':
+    javob= math.divide(a,b)        
